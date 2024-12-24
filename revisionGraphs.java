b580bc1699075bc96400f79c8e2e71ca2164adf8
code-Quester <neel.work03@gmail.com>
2024-12-25 01:43:59 +0530

    commited

--- revisionGraphs.java
+++ revisionGraphs.java
@@ -27,6 +27,88 @@ public class revisionGraphs {
             }
         }
     }
+    public static void dfs(ArrayList<Edge> graph[]){
+        boolean vis[] = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]) {
+                dfsUtil(graph, i, vis);
+            }
+        }
+    }
+    public static void dfsUtil(ArrayList<Edge> graph[],int curr,boolean vis[] ){
+        vis[curr]=true;
+        System.out.print(curr+" ");
+        for(int i=0;i<graph[curr].size();i++){
+            Edge e = graph[curr].get(i);
+            if (!vis[e.dest]) {
+                dfsUtil(graph, e.dest, vis);
+            }
+        }
+    }
+    public static boolean detectCycleDfs(ArrayList<Edge> graph[]){
+        boolean vis[] = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]) {
+                if(detectCycleDfsUtil(graph, i, vis, -1)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static boolean detectCycleDfsUtil(ArrayList<Edge> graph[],int curr,boolean vis[],int par ){
+        vis[curr]=true;
+        for(int i=0;i<graph[curr].size();i++){
+            Edge e = graph[curr].get(i);
+            if (!vis[e.dest]) {
+                if(detectCycleDfsUtil(graph, e.dest, vis,curr)){
+                    return true;
+                }
+            }else if (vis[e.dest] && par!=e.dest) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean hasPath(ArrayList<Edge> graph[], int src, int dest, boolean[]vis){
+        if (src==dest) {
+            return true;
+        }
+        vis[src]= true;
+        for (int i = 0; i < graph[src].size(); i++) {
+            Edge e = graph[src].get(i);
+            if(!vis[e.dest] && hasPath(graph, e.dest, dest, vis))return true;
+        }
+        return false;
+    }
+    public static boolean isBipartite(ArrayList<Edge> graph[]){
+        int col[]= new int[graph.length];
+        for (int i = 0; i < col.length; i++) {
+            col[i] =-1;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < col.length; i++) {
+            if (col[i]==-1) {
+                q.add(i);
+                col[i] = 0;
+                while (!q.isEmpty()) {
+                    int curr = q.remove();
+                    for (int j = 0; j < graph[curr].size(); j++) {
+                        Edge e = graph[curr].get(j);
+                        if (col[e.dest]==-1) {
+                            int nextCol = col[curr]==0?1:0;
+                            col[e.dest]= nextCol;
+                            q.add(e.dest);
+                        }
+                        else if(col[e.dest]==col[curr]){
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         int v=7;
         ArrayList<Edge> graph []= new ArrayList[v];
@@ -43,11 +125,11 @@ public class revisionGraphs {
         graph[2].add(new Edge(2, 4, 0));
 
         graph[3].add(new Edge(3, 1, 0));
-        graph[3].add(new Edge(3, 4, 0));
+        // graph[3].add(new Edge(3, 4, 0));
         graph[3].add(new Edge(3, 5, 0));
 
         graph[4].add(new Edge(4, 2, 0));
-        graph[4].add(new Edge(4, 3, 0));
+        // graph[4].add(new Edge(4, 3, 0));
         graph[4].add(new Edge(4, 5, 0));
 
         graph[5].add(new Edge(5, 3, 0));
@@ -57,6 +139,12 @@ public class revisionGraphs {
         graph[6].add(new Edge(6, 5, 0));
 
         //bfs
-        bfs(graph);
+        // bfs(graph);
+        // System.out.println();
+        // dfs(graph,0,new boolean[graph.length]);
+        // System.out.println(hasPath(graph, 1, 6 , new boolean[graph.length]));
+        dfs(graph);
+        // System.out.println("Cycle exists : "+detectCycleDfs(graph));
+        System.out.println(isBipartite(graph));
     }
 }
